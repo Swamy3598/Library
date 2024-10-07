74e91c37023e4802864df5b87df3ed4cfede7645
Swamy3598 <157398627+Swamy3598@users.noreply.github.com>
2024-10-07 20:24:04 +0530

    Add files via upload

---
+++ Library-master/Library-master/src/components/CategoriesData/CategoriesData.js
@@ -0,0 +1,35 @@
+const categories = [
+     {
+          categoryUrl: "../MenuProductData/CSE",
+          categoryImage: "https://static.vecteezy.com/system/resources/previews/020/408/768/original/cse-letter-logo-design-on-white-background-cse-creative-circle-letter-logo-concept-cse-letter-design-vector.jpg",
+          categoryName: "CSE",
+          
+     },
+     {
+          categoryUrl: "../MenuProductData/ECE",
+          categoryImage: "https://th.bing.com/th/id/OIP.5ALTA_-3jprVmONgsJhPKQHaHa?rs=1&pid=ImgDetMain",
+          categoryName: "ECE",
+          
+     },
+     {
+          categoryUrl: "../MenuProductData/MECH",
+          categoryImage: "https://th.bing.com/th/id/OIP.VJOnxfwdIqXM572sFGd8HAHaEo?rs=1&pid=ImgDetMain",
+          categoryName: "MECH",
+          
+     },
+     {
+          categoryUrl: "../MenuProductData/EEE",
+          categoryImage: "https://as2.ftcdn.net/v2/jpg/04/54/74/29/1000_F_454742990_HyACt36Iem2aQTJteA4upyF4GjPB24EJ.jpg",
+          categoryName: "EEE",
+          
+     },
+     {
+          categoryUrl: "../MenuProductData/CIVIL",
+          categoryImage: "https://logodix.com/logo/316314.png",
+          categoryName: "CIVIL",
+          
+     },
+     
+
+];
+export default categories;
